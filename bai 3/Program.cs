b04d69e5148daf7f2844b74e6feed381264e1fce
MAY05 <hienne890@gmail.com>
2023-03-09 17:02:54 +0700

    .

---
+++ bai 3/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace _1
+{
+    class Program
+    {
+        #region 1
+        //static void bt1(string s)
+        //{
+        //    for (int i = 0; i<s.Length;i++)
+        //    {
+        //        Console.Write($" {s[i]}");
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    bt1("logitech");
+
+        //    Console.ReadKey();
+        //}
+        #endregion
+
+        #region 2 
+        //static void bt2(string s)
+        //{
+        //    for (int i = s.Length-1; i>=0; i--)
+        //    {
+        //            Console.Write($" {s[i]}");
+        //        }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    bt2("logitech");
+
+        //    Console.ReadKey();
+        //}
+        #endregion
+
+        #region 3
+
+        #endregion
+    }
+}
